2ba1c6a3eef0204308e6f30ba16fb40000fc0bf6
SaipavanMaddala <sai984942@gmail.com>
2023-05-31 15:00:10 +0530

    addition

---
+++ src/com/sai/add/Add.java
@@ -0,0 +1,11 @@
+package com.sai.add;
+
+public class Add {
+   public static void main(String[] args) {
+	int num1=10;
+	int num2=20;
+	int res;
+	res= num1+num2;
+	System.out.println("addition of two number-->"+res);
+}
+}
